60394f6be153fde17b5ec8b02b609d9eabbef4b8
DanilPomortsev <90237537+DanilPomortsev@users.noreply.github.com>
2023-03-17 22:30:46 +0300

    Update 2_4.py

--- 2_4.py
+++ 2_4.py
@@ -1,120 +1,115 @@
 import numpy as np
 import math
+import warnings
 
-def diagonal_preobl(matrix):
-    n = matrix.shape[0]
+warnings.filterwarnings('ignore')
+
+def ItrTransformation(A, b):# перевод уравнения в вид: x = Bx + c
+    n = A.shape[0]
+    A_ = np.zeros((n,n),dtype='float64')# матрица B
+    b_ = np.zeros(n, dtype='float64')# вектор столбец с
+    for i in range(n):# далее просто расставляю значения в соответсвии с коэфицентами
+        b_[i] = b[i] / A[i][i]
     for i in range(n):
-        sum = 0
         for j in range(n):
-            if j != i:
-                sum += abs(matrix[i, j])
-        if sum > abs(matrix[i, i]):
+            if(i == j):
+                A_[i][j] = 0
+            else:
+                A_[i][j] = -A[i][j]/A[i][i]
+    return (A_, b_)
+
+def Jacobi(A, b, eps):# метод Якоби
+    n = A.shape[0]
+    cur_eps = eps + 1 # разница между знацениями в предыдущей и текущей итерации
+    (B, c) = ItrTransformation(A,b) # преобразовываем
+    X_result = np.array([0.0]*n, dtype='float64')
+    if(np.linalg.norm(B) < 1):# если количество итераций оценимо заранее, оцениваем
+        apriorEstimate = math.ceil(math.log(eps, math.e) - math.log(np.linalg.norm(c), math.e) + math.log(1-np.linalg.norm(B), math.e)/math.log(np.linalg.norm(B), math.e))# просто формула
+    else:
+        apriorEstimate = 0
+
+    number_of_iteration = 0
+    while cur_eps > eps:
+        X_result_past = X_result.copy()# сохраняем значение x в пердидущей итерации
+        X_result = np.dot(B, X_result_past) + c# вычисляем значение x в текущей итерации
+        number_of_iteration += 1
+        arr_eps = abs(X_result - X_result_past)# вычисляем разницу между итерациями
+        cur_eps = max(arr_eps)
+        if number_of_iteration == 1000:
+            return False
+    return (X_result, apriorEstimate, number_of_iteration)
+
+def Seidel(A, b, eps):# метод Зейдоля
+    n = A.shape[0]# то же самое что в предидущем методе
+    cur_eps = eps + 1
+    (B, c) = ItrTransformation(A,b)
+    X_result = np.array([0.0]*n, dtype='float64')
+    if(np.linalg.norm(B) < 1):
+        apriorEstimate = math.ceil(math.log(eps, math.e) - math.log(np.linalg.norm(c), math.e) + math.log(1-np.linalg.norm(B), math.e)/math.log(np.linalg.norm(B), math.e))
+    else:
+        apriorEstimate = 0
+
+    number_of_iteration = 0
+    while cur_eps > eps:
+        X_result_past = X_result.copy()
+        for i in range(n):# то же самое что в прошлом методе только, теперь мы используем только что вычисленное значение x
+            X_result[i] = c[i]
+            for j in range(n):
+                X_result[i] += B[i][j] * X_result[j]
+        number_of_iteration += 1
+        arr_eps = abs(X_result - X_result_past)
+        cur_eps = max(arr_eps)
+        if number_of_iteration == 1000:
+            return False
+
+    return (X_result, apriorEstimate, number_of_iteration)
+
+def DiagDominMatrGener(size):# генератор матрицы с диагональной доминацией
+    check_matrix = np.random.randint(0,size=(size, size),high=100)# генерируем случайную матрицу
+    while np.linalg.det(check_matrix) == 0:
+        check_matrix = np.random.randint(0,size=(size, size),high=100)
+    n = check_matrix.shape[0]
+    for i in range(n):
+        for j in range(n):
+            if(i != j):
+                check_matrix[i, i] += abs(check_matrix[i, j])# прибавлеем все мтрочные элементы к диагональному элементу, чтобы создать диагональную доминацию
+    return check_matrix
+
+def IsDiagDominMat(A):# проверка матрицы на диагональную доминацию
+    n = A.shape[0]
+    for i in range(n):
+        diag_el = A[i][i]
+        for j in range(n):
+            if(i != j):
+                diag_el -= A[i][j]
+        if(diag_el < 0):
             return False
     return True
 
-def matrixWithDiagonalDominance(n):                     #конструирование матрицы с диаг преобл.
-    matrix = 110*np.random.random_sample((n,n)) - 30
+def IsPosDef(x):# проверка на положительность матрицы
+    return np.all(np.linalg.eigvals(x) > 0)
 
-    for i in range(n):
-        sum = 0.0
-        for j in range(n):
-            sum += abs(matrix[i, j])
-        matrix[i, i] = sum
+def PosNonDiagDomMatGen(size):# генерируем положительную матрицу без диагональной доминации
+    check_matrix = np.random.randint(0,size=(size, size),high=100)
+    while ((np.linalg.det(check_matrix) == 0) and (IsDiagDominMat(check_matrix)) and (IsPosDef(check_matrix))):
+        check_matrix = np.random.randint(0,size=(size, size),high=100)
+    return check_matrix
 
-    return matrix
+def AnwserGen(size):# генерируем ветор ответов
+    anwser = np.random.randint(0,size=(size, 1),high=100)
+    return anwser
 
+# проверяем
+n = 3
+diag = DiagDominMatrGener(n)
+pol = PosNonDiagDomMatGen(n)
+anwser_1 = AnwserGen(n)
+anwser_2 = AnwserGen(n)
+anwser_3 = AnwserGen(n)
+anwser_4 = AnwserGen(n)
 
-def Decomposition(a, b):              # перевод уравнения в вид: x = Bx + c
-    a = a.copy()
-    n = np.size(a, 0)
+print(Jacobi(diag, anwser_1, 0.000001))
+print(Jacobi(pol, anwser_2, 0.000001))
 
-    l = np.zeros((n, n), dtype='float64')
-    invD = np.zeros((n, n), dtype='float64')
-    r = np.zeros((n, n), dtype='float64')
-
-    for i in range(n):
-        invD[i, i] = 1.0 / a[i, i]
-
-    for i in range(n):
-        for j in range(0, i):
-            l[i, j] = a[i, j]
-
-    for i in range(n):
-        for j in range(i + 1, n):
-            r[i, j] = a[i, j]
-
-    return (np.dot(-invD, l + r), np.dot(invD, b))
-
-def Jacobi(a, b, eps = 0.000001):
-    (B, c) = Decomposition(a, b)
-    q = np.linalg.norm(B, 1)
-    x = c.copy()                #начальное значение: x = с
-    prevX = x.copy()
-    x = np.dot(B, prevX) + c
-    aprioriEstimate = math.ceil(math.log( eps * np.linalg.norm(x - prevX, 1), q ))
-
-    threshold = (1.0 - q) / q * eps
-
-    index = 0
-    while np.linalg.norm(x - prevX, 1) > threshold:
-        index += 1
-        prevX = x.copy()
-        x = np.dot(B, x) + c
-
-    return (x, aprioriEstimate, index)
-
-
-def Seidel(a, b, eps = 0.000001):
-    n = np.size(a, 0)
-    (B, c) = Decomposition(a, b)
-    q = np.linalg.norm(B, 1)
-    x = c.copy()
-    prevX = x.copy()
-    x = np.dot(B, prevX) + c
-    aprioriEstimate = math.ceil(math.log(eps * np.linalg.norm(x - prevX, 1), q))
-
-    threshold = (1.0 - q) / q * eps
-
-    index = 0
-    while np.linalg.norm(x - prevX, 1) > threshold:
-        index += 1
-        prevX = x.copy()
-        for i in range(n):
-            newValue = c[i, 0]
-            for j in range(n):
-                newValue += B[i, j] * x[j, 0]
-            x[i, 0] = newValue
-
-    return (x, aprioriEstimate, index)
-
-
-size_of_matrix = 5
-b = 110*np.random.random_sample((size_of_matrix,1)) - 30
-#b = np.array([[1],[2],[2]])
-# matrix = 110*np.random.random_sample((size_of_matrix,size_of_matrix)) - 30
-# while np.linalg.det(matrix) == 0:
-#     matrix = 110*np.random.random_sample((size_of_matrix,size_of_matrix)) - 30
-#matrix = np.array([[0,6,10],[-3,-7,2],[5,-1,5]])
-matrix = matrixWithDiagonalDominance(size_of_matrix)
-
-print(matrix)
-
-print("Метод Якоби")
-tmp = Jacobi(matrix, b)
-print('решение')
-print(tmp[0])
-print('Априорная оценка')
-print(tmp[1])
-print('Апостериорная оценка')
-print(tmp[2])
-
-print("===============")
-
-print("Метод Зейделя")
-tmp = Seidel(matrix, b)
-print('решение')
-print(tmp[0])
-print('Априорная оценка')
-print(tmp[1])
-print('Апостериорная оценка')
-print(tmp[2])
+print(Seidel(diag, anwser_3, 0.000001))
+print(Seidel(pol, anwser_4, 0.000001))
